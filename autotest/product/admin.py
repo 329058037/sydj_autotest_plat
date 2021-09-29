86fb5e2dda2eec47ea959da220f103d4956bee74
329058037@qq.com <329058037@qq.com>
2021-09-29 10:42:00 +0800

    0929代码更新

--- autotest/product/admin.py
+++ autotest/product/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from product.models import product
+from product.models import Product
 
 # Register your models here.
 
 class ProductAdmin(admin.ModelAdmin):
     list_display = ['productname', 'productdesc', 'producter', 'create_time', 'id']
-
-    admin.site.register(product)
+# 把产品模块注册到Django damin后台并显示
+admin.site.register(Product)
--- autotest/product/models.py
+++ autotest/product/models.py
@@ -2,21 +2,21 @@ from django.db import models
 
 # Create your models here.
 
-class product(models.Model):
+class Product(models.Model):
     # 产品名称
     productname = models.CharField('产品名称', max_length=64)
     # 产品描述
     productdesc = models.CharField('产品描述', max_length=200)
     # 产品负责人
-    productder = models.CharField('产品负责人', max_length=200)
+    producter = models.CharField('产品负责人', max_length=200)
     # 创建时间 自动获取 当前时间
     create_time = models.DateTimeField('创建时间', auto_created=True)
 
-class Meta:
-    # 设置迁移后的表名
-    db_table = "product"
-    verbose_name = '产品管理'
-    verbose_name_plural = '产品管理'
+    class Meta:
+        # 设置迁移后的表名
+        # db_table = "Product"
+        verbose_name = '产品管理'
+        verbose_name_plural = verbose_name
 
     def __str__(self):
         return self.productname
